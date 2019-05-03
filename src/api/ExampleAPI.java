890b3574ede88fd4da0d69ea7bf532a390aa0fd2
Robert Chen <robertchen@live.com>
2019-05-02 17:52:50 -0700

    Uploaded Example API

---
+++ src/api/ExampleAPI.java
@@ -0,0 +1,20 @@
+package api;
+
+/**
+ * An example API.
+ * 
+ * @author s-chenrob
+ *
+ */
+public class ExampleAPI {
+	/**
+	 * Each method should have some documentation.
+	 * 
+	 * https://xkcd.com/221/
+	 * 
+	 * @return a random number
+	 */
+	public static int getRandomNumber() {
+		return 4; // chosen by fair dice roll.
+	}
+}
